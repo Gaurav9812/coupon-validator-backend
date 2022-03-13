d0660383036dbd2e4de93047b61b50a32c1acd9e
Gaurav mehra <gauravmehra1298@gmail.com>
2022-03-13 10:57:00 +0530

    fin

--- Routes/index.js
+++ Routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const coupon_controller = require("../Controllers/Coupon-controller");
 //if url has product
 router.get("/", function (req, res) {
-  return res.send(<h1>All coupons</h1>);
+  return res.end("<h1>Backend of Coupon Validator</h1>");
 });
 router.post("/create", coupon_controller.create);
 router.get("/getCoupon", coupon_controller.getCoupons);
